0e032ec6b089e2cf9e35ab88368b16f692cae1ef
Koji Abe <gracehime@gmail.com>
2023-05-25 21:46:37 +0900

    p52 リスト3.3を実装した。状態遷移を担当するメソッドを実装した。

--- ch3/sample.rb
+++ ch3/sample.rb
@@ -5,6 +5,18 @@ class Sample
         @attr_b = true
     end
 
-    def play
+    def play(evt, param)
+        puts "#{@state} ->"
+        puts "  event:#{evt}, param: #{param}"
+        case @state
+        when :ST0
+            st0_proc(evt, param)
+        when :ST1
+            st1_proc(evt, param)
+        when :ST2
+            # none
+        end
+        puts "          -> #{@state}"
+        puts 'finished.' if @state == :ST2
     end
 end
